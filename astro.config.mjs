a4959018cf3697b874fc5ab2e26f63df97cb9971
oscar-richmond <oscar@selfhood-studios.com>
2026-04-22 00:24:20 +0100

    Update contact form colours and emails

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://oscar-richmond.github.io',
-  base: '/beyond-the-badge-1',
+  site: 'https://beyondthebadge.co.uk', // update once you have your Vercel domain
 });
